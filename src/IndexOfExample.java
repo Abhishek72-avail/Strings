883f6827d75221e0b0f4ec398373e14cc4a58e23
Abhishek72-avail <121235088+Abhishek72-avail@users.noreply.github.com>
2023-02-03 15:42:08 +0530

    test

--- src/IndexOfExample.java
+++ src/IndexOfExample.java
@@ -4,7 +4,7 @@ public class IndexOfExample {
         public static void main(String args[]){
             String s1="this is index of example";
 //passing substring
-            int index1=s1.indexOf("is");//returns the index of is substring  
+            int index1=s1.indexOf("is");//returns the index of is substring
             int index2=s1.indexOf("index");//returns the index of index substring
             System.out.println(index1+"  "+index2);//2 8
 
